a918de19b03df2282893b6ffdcf8b87fe37b8386
anthod0 <91065936+ocherry341@users.noreply.github.com>
2026-09-10 21:40:27 +0800

    configure delta for git diffs

--- .chezmoiscripts/run_once_after_setup-git-config.sh
+++ .chezmoiscripts/run_once_after_setup-git-config.sh
@@ -10,3 +10,8 @@ fi
 git config --global init.defaultBranch "main"
 git config --global pull.rebase true
 git config --global core.excludesfile "$HOME/.gitignore_global"
+git config --global core.pager delta
+git config --global interactive.diffFilter "delta --color-only"
+git config --global delta.navigate true
+git config --global delta.light true
+git config --global merge.conflictStyle zdiff3
